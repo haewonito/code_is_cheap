eb75faa0ed054fdcec4693a964d47356619c4a46
Haewon Jeon <haewonito@gmail.com>
2021-09-04 07:48:39 -0600

    Initial Commit

---
+++ lib/text.rb
@@ -0,0 +1,26 @@
+class Text
+
+  def initialize
+  end
+
+  def welcome
+    puts "Welcome to MASTERMIND\n\n"
+    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def quit
+    puts "That's too bad. Next time."
+  end
+
+  def ask_for_guess
+    puts "I have generated a beginner sequence with four elements made up of:"\
+    "(r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the"\
+    " game."
+    puts "What's your guess?"
+  end
+
+  def instruction
+    puts "Let's worry about instruction later."
+  end
+
+end
